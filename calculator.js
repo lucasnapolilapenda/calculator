e62369c25f39b1177ad714c33eaf5740cdeedb48
Lucas Napoli <lucasnapoli@wattopoststudio.com>
2019-07-14 19:52:42 -0400

    Fully Functional Calculator

--- calculator.js
+++ calculator.js
@@ -31,6 +31,9 @@ function getNumber(num) {
         case 0:
             input_var.value += '0';
             break;
+        case '.':
+            input_var.value += '.';
+            break;
     }
 }
 
@@ -56,27 +59,17 @@ function getOperand(operand) {
         case '/':
             input_var.value += '/';
             break;
-        case '+/-':
-            input_var.value += '-' + input_var.value;
+        case '%':
+            input_var.value += '%';
             break;
-    }
-
-}
-
-function backspace() {
-    var input_var = document.getElementById('input');
-    var x = input_var.value;
-    if (x.length > 0) {
-        x = x.substring(0, x.length-1);
-        input_var.value = x;
     }
 
 }
 
 function compute() {
     var input_var = document.getElementById('input');
-    ans = Math.floor(+eval(input_var.value));
-    document.getElementById('answer').value = '=' + ans;
+    ans = eval(input_var.value);
+    document.getElementById('input').value =  ans.toFixed(2);
 
 }
 
