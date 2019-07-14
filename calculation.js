6e787a0ceefdbe1ef455035e50703347a951ea84
Lucas Napoli <lucasnapoli@wattopoststudio.com>
2019-07-14 12:25:08 -0400

    Menu first design

--- calculation.js
+++ calculation.js
@@ -79,3 +79,15 @@ function compute() {
     document.getElementById('answer').value = '=' + ans;
 
 }
+
+var i = 0;
+function getBrackets () {
+    var input_var = document.getElementById('input')
+    if (i == 0) {
+        input_var.value += '(';
+        i = 1;
+    }else if (i == 1) {
+        input_var.value += ')';
+        i = 0
+    }
+}
